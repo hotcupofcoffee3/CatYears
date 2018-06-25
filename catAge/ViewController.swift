2ea840fae6fd7a6e37d30f1337ea85fe81cee5fd
Adam Moore <adammoore@Adams-MacBook-Pro.local>
2018-06-25 15:58:53 -0600

    Initial commit

--- catAge/ViewController.swift
+++ catAge/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var age: UITextField!
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        
+        if let retrievedAge = age.text {
+            if retrievedAge != "" {
+                if let ageAsInt = Int(retrievedAge) {
+                    let ageInCatYears = ageAsInt * 7
+                    ageInCatYearsLabel.text = "You are \(ageInCatYears) years old in cat years."
+                }
+            } else {
+                ageInCatYearsLabel.text = "You are 0 years old in cat years!"
+            }
+        }
+    }
+    
+    @IBOutlet weak var ageInCatYearsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
